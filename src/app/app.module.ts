7dc36ac142e62a7a775dd6f04e730299904f64f9
Kim Do <kim.h.do.seven@gmail.com>
2019-02-06 09:20:39 -0800

    init

---
+++ src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { NgtUniversalModule } from '@ng-toolkit/universal';
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import { TransferHttpCacheModule } from '@nguniversal/common';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports:[
+ CommonModule,
+NgtUniversalModule,
+ 
+ TransferHttpCacheModule,
+HttpClientModule,
+ 
+    
+    AppRoutingModule
+  ],
+  providers: [],
+})
+export class AppModule { }
